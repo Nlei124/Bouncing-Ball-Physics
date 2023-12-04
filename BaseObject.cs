9c0d86d8b7673b35de77a644118a64671b85e1a1
Nelson Lei <nzl4096@rit.edu>
2023-12-03 20:46:17 -0500

    Add project files.

---
+++ BaseObject.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Bouncing_Ball_Physics
+{
+    class BaseObject
+    {
+        
+        private static int WindowW = 800;
+        private static int WindowH = 480;
+        //fields
+        private Texture2D circleTexture;
+        private Vector2 curPosition;
+        private double radius;
+        private Vector2 velocity;
+        //private Vector2 acceleration;
+        private float redness;
+
+        public BaseObject(Texture2D texture, Vector2 pos, double r)
+        {
+            circleTexture = texture;
+            curPosition = pos;
+            radius = r;
+            velocity = new Vector2(0, 0);
+            //acceleration = new Vector2(0, 0);
+            redness = 0;
+        }
+
+        public BaseObject(Texture2D texture, float xPos, float yPos, double r)
+        {
+            circleTexture = texture;
+            curPosition = new Vector2(xPos, yPos);
+            radius = r;
+            velocity = new Vector2(0, 0);
+            //acceleration = new Vector2(0, 0);
+            redness = 0;
+        }
+        public BaseObject(Texture2D texture, float xPos, float yPos, double r, Vector2 vel)
+        {
+            circleTexture = texture;
+            curPosition = new Vector2(xPos, yPos);
+            radius = r;
+            velocity = vel;
+            //acceleration = acc;
+            redness = 0;
+        }
+
+        public double X
+        {
+            get { return curPosition.X; }
+        }
+        public double Y
+        {
+            get { return curPosition.Y; }
+        }
+        public double Mass
+        {
+            get { return radius * radius; }
+        }
+        public double Energy
+        {
+            get { return 0.5 * Mass * velocity.LengthSquared(); }
+        }
+        public double Momentum
+        {
+            get { return Mass * velocity.Length(); }
+        }
+        public int WWidth
+        {
+            set { WindowW = value; }
+        }
+        public int WHeight
+        {
+            set { WindowH = value; }
+        }
+
+        public int Redness
+        {
+            get { return (int) redness; }
+        }
+
+        public void Draw(SpriteBatch sb)
+        {
+            sb.Draw(circleTexture,
+                new Rectangle((int)(curPosition.X - radius), (int)(curPosition.Y - radius)
+                , (int)(radius * 2), (int)(radius * 2))
+                , new Color((int)redness, 10, 10));
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            Vector2 prevPosition = curPosition;
+            UpdatePosition(gameTime.ElapsedGameTime.TotalSeconds);
+            curPosition = KeepInBounds(prevPosition, gameTime.ElapsedGameTime.TotalSeconds);
+            redness -= (float) gameTime.ElapsedGameTime.TotalSeconds * 100;
+        }
+        public void UpdatePosition(double time)
+        {
+            //velocity.X += (float)(acceleration.X * time);
+            //velocity.Y += (float)(acceleration.Y * time);
+            curPosition.X += (float)(velocity.X * time);
+            curPosition.Y += (float)(velocity.Y * time);
+        }
+
+        public Vector2 KeepInBounds(Vector2 prevPos, double time)
+        {
+            if (curPosition.X - radius < 0)
+            {
+                //IncreaseRedness();
+                #region comment
+                /*
+                double distFromWall = 0 - (prevPos.X - radius);
+                //Use quadratic formula to get the time it took to hit the wall
+                double timeToWall = (-velocity.X + Math.Sqrt(velocity.X * velocity.X
+                    - 4 * 0.5 * acceleration.X * -distFromWall))
+                    / (0.5 * acceleration.X);
+                curPosition = prevPos;
+                UpdatePosition(timeToWall);
+                velocity.X *= -1;
+                UpdatePosition(time - timeToWall);
+                */
+                #endregion
+                double distFromWall = 0 - (prevPos.X - radius);
+                double timeToWall = distFromWall / velocity.X;
+                curPosition = prevPos;
+                UpdatePosition(timeToWall);
+                velocity.X *= -1;
+                Vector2 lp = curPosition;
+                UpdatePosition(time - timeToWall);
+                return KeepInBounds(lp, time - timeToWall);
+            }
+            if (curPosition.X + radius > WindowW)
+            {
+                //IncreaseRedness();
+                #region commented out
+                /*
+                double distFromWall = WindowW - (prevPos.X + radius);
+                //Use quadratic formula to get the time it took to hit the wall
+                double timeToWall = (-velocity.X + Math.Sqrt(velocity.X * velocity.X 
+                    - 4 * 0.5 * acceleration.X * -distFromWall))
+                    / (0.5 * acceleration.X);
+                curPosition = prevPos;
+                UpdatePosition(timeToWall);
+                velocity.X *= -1;
+                UpdatePosition(time - timeToWall);
+                */
+                #endregion
+                double distFromWall = WindowW - (prevPos.X + radius);
+                double timeToWall = distFromWall / velocity.X;
+                curPosition = prevPos;
+                UpdatePosition(timeToWall);
+                velocity.X *= -1;
+                Vector2 lp = curPosition;
+                UpdatePosition(time - timeToWall);
+                return KeepInBounds(lp, time - timeToWall);
+            }
+            if (curPosition.Y - radius < 0)
+            {
+                //IncreaseRedness();
+                double distFromWall = 0 - (prevPos.Y - radius);
+                double timeToWall = distFromWall / velocity.Y;
+                curPosition = prevPos;
+                UpdatePosition(timeToWall);
+                velocity.Y *= -1;
+                Vector2 lp = curPosition;
+                UpdatePosition(time - timeToWall);
+                return KeepInBounds(lp, time - timeToWall);
+            }
+            if (curPosition.Y + radius > WindowH)
+            {
+                //IncreaseRedness();
+                double distFromWall = WindowH - (prevPos.Y + radius);
+                double timeToWall = distFromWall / velocity.Y;
+                curPosition = prevPos;
+                UpdatePosition(timeToWall);
+                velocity.Y *= -1;
+                Vector2 lp = curPosition;
+                UpdatePosition(time - timeToWall);
+                return KeepInBounds(lp, time - timeToWall);
+            }
+            else
+            {
+                return curPosition;
+            }
+        }
+
+        public void CollideWith(BaseObject other)
+        {
+
+            IncreaseRedness();
+            other.IncreaseRedness();
+            
+            Vector2 normalVector = other.curPosition - curPosition;
+            normalVector.Normalize();
+            Vector2 tanVector = new Vector2(-normalVector.Y, normalVector.X);
+
+            float INormVel = Vector2.Dot(normalVector, velocity);
+            float IOtherNormVel = Vector2.Dot(normalVector, other.velocity);
+
+            Vector2 FNormVel = Vector2.Multiply(normalVector, (float)(INormVel * (Mass - other.Mass) / (Mass + other.Mass) + IOtherNormVel * (Mass * 2) / (Mass + other.Mass)));
+            Vector2 FOtherNormVel = Vector2.Multiply(normalVector, (float)(IOtherNormVel * (other.Mass - Mass) / (Mass + other.Mass) + INormVel * (other.Mass * 2) / (Mass + other.Mass)));
+
+            velocity = FNormVel + Vector2.Multiply(tanVector, Vector2.Dot(tanVector, velocity));
+            other.velocity = FOtherNormVel + Vector2.Multiply(tanVector, Vector2.Dot(tanVector, other.velocity));
+
+            //Adding a "separating force"
+            //proximity is the sum of both radius - distance between centers 
+            double proximity = radius + other.radius - (Distance(curPosition, other.curPosition));
+            curPosition += Vector2.Multiply(curPosition-other.curPosition, (float)(.01 * proximity * Mass /(Mass + other.Mass)));
+            other.curPosition += Vector2.Multiply(other.curPosition- curPosition, (float) ( 0.01 * proximity * other.Mass / (Mass + other.Mass)));
+        }
+
+        public bool CheckCollide(BaseObject other)
+        {
+            if(radius + other.radius >= Distance(curPosition, other.curPosition))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public double Distance(Vector2 point1, Vector2 point2)
+        {
+            return Math.Sqrt(Math.Pow(point1.X - point2.X, 2) + Math.Pow(point1.Y - point2.Y, 2));
+        }
+
+        private void IncreaseRedness()
+        {
+            /*
+            if(redness < 255)
+            {
+                redness += 10;
+            }*/
+            redness = 200;
+        }
+    }
+}
---
+++ Game1.cs
@@ -0,0 +1,143 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bouncing_Ball_Physics
+{
+    
+    public class Game1 : Game
+    {
+        
+        public int WindowW;
+        public int WindowH;
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+        private List<BaseObject> balls;
+        private Texture2D circleT;
+        private SpriteFont font;
+        private float fontHeight;
+        private double energy;
+        private double momentum;
+        private MouseState prevMState;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+            Window.AllowUserResizing = true;
+            Window.ClientSizeChanged += OnResize;
+        }
+        public void OnResize(Object sender, EventArgs e)
+        {
+            if (balls[0] != null)
+            {
+                balls[0].WWidth = _graphics.GraphicsDevice.Viewport.Width;
+                balls[0].WHeight = _graphics.GraphicsDevice.Viewport.Height;
+            }
+            foreach (BaseObject b in balls)
+            {
+                b.WWidth = _graphics.GraphicsDevice.Viewport.Width;
+                b.WHeight = _graphics.GraphicsDevice.Viewport.Height;
+            }
+            WindowW = _graphics.GraphicsDevice.Viewport.Width;
+            WindowH = _graphics.GraphicsDevice.Viewport.Height;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            balls = new List<BaseObject>();
+            WindowW = _graphics.PreferredBackBufferWidth;
+            WindowH = _graphics.PreferredBackBufferHeight;
+            //_graphics.PreferredBackBufferWidth = WindowW;
+            //_graphics.PreferredBackBufferHeight = WindowH;
+            //_graphics.ApplyChanges();
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+            circleT = Content.Load<Texture2D>("Circle");
+            
+            balls.Add(new BaseObject(circleT, 50, 300, 50, new Vector2(120, 50)));
+            balls.Add(new BaseObject(circleT, 300, 400, 20, new Vector2(100, 0)));
+            /*balls.Add(new BaseObject(circleT, 500, 100, 20, new Vector2(100, 500)));
+            balls.Add(new BaseObject(circleT, 300, 200, 50, new Vector2(300, 500)));
+            balls.Add(new BaseObject(circleT, 300, 20, 40, new Vector2(300, 300)));
+            balls.Add(new BaseObject(circleT, 350, 20, 15, new Vector2(700, 600)));
+            balls.Add(new BaseObject(circleT, 350, 100, 15, new Vector2(300, 600)));
+            balls.Add(new BaseObject(circleT, 500, 200, 30, new Vector2(-300, 600)));
+            */
+            
+
+
+            font = Content.Load<SpriteFont>("ArialRound15");
+            fontHeight = font.MeasureString("A").Y;
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+            MouseState mState = Mouse.GetState();
+            if(mState.LeftButton == ButtonState.Pressed && prevMState.LeftButton == ButtonState.Released)
+            {
+                balls.Add(new BaseObject(circleT, mState.X, mState.Y, 30, new Vector2()));
+            }
+
+            // TODO: Add your update logic here
+            foreach (BaseObject b in balls)
+            {
+                b.Update(gameTime);
+            }
+            for (int i = 0; i < balls.Count; i++)
+            {
+                
+                for (int j = i+1; j < balls.Count; j++)
+                {
+                    if (balls[i].CheckCollide(balls[j]))
+                    {
+                        balls[i].CollideWith(balls[j]);
+                        //TODO: try to flag collided objects
+                        //and resolve collisions in later step
+                    }
+                   
+                }
+            }
+            prevMState = mState;
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+            _spriteBatch.Begin();
+            energy = 0;
+            momentum = 0;
+
+            foreach (BaseObject b in balls)
+            {
+                b.Draw(_spriteBatch);
+                energy += b.Energy;
+                momentum += b.Momentum;
+            }
+
+            //_spriteBatch.DrawString(font, String.Format(balls[0].X.ToString()), new Vector2(), Color.Black);
+            _spriteBatch.DrawString(font, String.Format("Ball 1 Redness" + balls[0].Redness.ToString()), new Vector2(), Color.Black);
+            _spriteBatch.DrawString(font, String.Format("KE: {0:F4}", energy/10000000), new Vector2(0, fontHeight), Color.Black);
+            _spriteBatch.DrawString(font, String.Format("Mom: {0:F4}", momentum/10000), new Vector2(0, fontHeight*2), Color.Black);
+
+            //MouseState mState = Mouse.GetState();
+            //_spriteBatch.Draw(circleT, new Rectangle(mState.X - 25, mState.Y -25, 50, 50), Color.White);
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
